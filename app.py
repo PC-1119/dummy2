ee81b15b350372cb481a43a64e177812e1b4a8cc
PC-1119 <chevvaprathyusha9550@gmail.com>
2025-11-12 22:59:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+# hello.py
+from flask import Flask
+
+# Create a Flask app
+app = Flask(__name__)
+
+# Define a route for the root URL
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
